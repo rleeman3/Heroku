584936a81f433f8bde344bbc786a389e7cd1f4ce
Rebecca Leeman <Leeman@umich.edu>
2015-07-06 23:07:54 +0200

    HELP ME PROCEED even tho basics basically r done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,26 @@ class PostsController < ApplicationController
 	before_action :authenticate_user!, except: [:show, :index]
 
 #we want only current user to see/be able to crud posts.
-  def set_user
-    @user = current_user
-    @post = @post.user
-  end
+  # def set_user
+  #   @user = current_user
+  #   @post = @post.user
+  # end
 
   def index
   	@posts = Post.all
   end
 
   def new
-  	@post = Post.new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -25,6 +34,12 @@ class PostsController < ApplicationController
 
   def update
   	@post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  validates :restaurant_name, :start_time, :end_time, :deal, presense: true
+  validates :restaurant_name, :start_time, :end_time, :deal, presence: true
   belongs_to :user
 end
